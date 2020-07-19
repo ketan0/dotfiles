58ef7d2d77c02550f9c451950783603d238bc335
Ketan Agrawal <agrawalk@stanford.edu>
2020-07-19 16:45:55 -0400

    cleanup

--- doom.d/config.el
+++ doom.d/config.el
@@ -224,7 +224,7 @@
            "* DONE %?") ;;TODO: put CLOSED + timestamp
           ("c" "coronavirus" entry (file+datetree
                                     (concat org-directory "20200314210447_coronavirus.org"))
-           "* %^{Heading}")))
+           "* %^{Heading}"))))
 
 (use-package! org-super-agenda)
 ;;   :defer t
@@ -243,6 +243,9 @@
         org-journal-carryover-items nil
         org-journal-date-format "%A, %d %B %Y"))
 
+(use-package! org-ql)
+(use-package! om)
+
 (use-package! org-roam
   :init
   (map! :map doom-leader-map "r" 'org-roam-find-file)
@@ -253,12 +256,16 @@
   (setq org-roam-graph-viewer nil)
   (setq org-roam-directory (concat ketan0/tramp-prefix org-directory)))
 
+(setq ketan0/org-thoughtset-package-path (concat ketan0/tramp-prefix "/Users/ketanagrawal/emacs-packages/org-thoughtset"))
+(use-package! org-thoughtset
+  :load-path ketan0/org-thoughtset-package-path)
 
-  (defun ketan0/latex-mode-setup ()
-    (setq-local company-backends
-                (push '(company-math-symbols-latex company-latex-commands)
-                      company-backends))
-    (push '(?$ . ("$ " . " $")) evil-surround-pairs-alist)))
+
+(defun ketan0/latex-mode-setup ()
+  (setq-local company-backends
+              (push '(company-math-symbols-latex company-latex-commands)
+                    company-backends))
+  (push '(?$ . ("$ " . " $")) evil-surround-pairs-alist))
 
 (use-package! tex
   :defer t
@@ -354,7 +361,6 @@
   :config
   (setq rtags-tramp-enabled t))
 
-(use-package! org-ql)
 
 ;; shamelessly taken from magnars
 (defun toggle-window-split ()
@@ -382,138 +388,9 @@
           (select-window first-win)
           (if this-win-2nd (other-window 1))))))
 
-(use-package! om)
 
 (use-package! request)
-
-;; (setq ketan0/org-thoughtset-package-path (concat ketan0/tramp-prefix "/Users/ketanagrawal/emacs-packages/org-thoughtset"))
-;; (use-package! org-thoughtset
-;;   :load-path ketan0/org-thoughtset-package-path)
 
 (use-package! dash)
 (use-package! s)
 (use-package! f)
-
-(defun my-org-display-inline-images (&optional include-linked refresh beg end)
-  "Display inline images.
-
-An inline image is a link which follows either of these
-conventions:
-
-  1. Its path is a file with an extension matching return value
-     from `image-file-name-regexp' and it has no contents.
-
-  2. Its description consists in a single link of the previous
-     type.
-
-When optional argument INCLUDE-LINKED is non-nil, also links with
-a text description part will be inlined.  This can be nice for
-a quick look at those images, but it does not reflect what
-exported files will look like.
-
-When optional argument REFRESH is non-nil, refresh existing
-images between BEG and END.  This will create new image displays
-only if necessary.  BEG and END default to the buffer
-boundaries."
-  (interactive "P")
-  (when (display-graphic-p)
-    (unless refresh
-      (org-remove-inline-images)
-      (when (fboundp 'clear-image-cache) (clear-image-cache)))
-    (org-with-wide-buffer
-     (goto-char (or beg (point-min)))
-     (let ((case-fold-search t)
-	   (file-extension-re (org-image-file-name-regexp)))
-       (while (re-search-forward "[][]\\[\\(?:file\\|[./~]\\)" end t)
-	 (let ((link (save-match-data (org-element-context))))
-	   ;; Check if we're at an inline image.
-	   (when (and (equal (org-element-property :type link) "file")
-		      (or include-linked
-			  (not (org-element-property :contents-begin link)))
-		      (let ((parent (org-element-property :parent link)))
-			(or (not (eq (org-element-type parent) 'link))
-			    (not (cdr (org-element-contents parent)))))
-		      (org-string-match-p file-extension-re
-					  (org-element-property :path link)))
-	     (let ((file (substitute-in-file-name (expand-file-name (org-element-property :path link)))))
-	       (when (file-exists-p file)
-		 (let ((width
-			;; Apply `org-image-actual-width' specifications.
-			(cond
-			 ((not (image-type-available-p 'imagemagick)) nil)
-			 ((eq org-image-actual-width t) nil)
-			 ((listp org-image-actual-width)
-			  (or
-			   ;; First try to find a width among
-			   ;; attributes associated to the paragraph
-			   ;; containing link.
-			   (let ((paragraph
-				  (let ((e link))
-				    (while (and (setq e (org-element-property
-							 :parent e))
-						(not (eq (org-element-type e)
-							 'paragraph))))
-				    e)))
-			     (when paragraph
-			       (save-excursion
-				 (goto-char (org-element-property :begin paragraph))
-				   (when
-				       (re-search-forward
-					"^[ \t]*#\\+attr_.*?: +.*?:width +\\(\\S-+\\)"
-					(org-element-property
-					 :post-affiliated paragraph)
-					t)
-				     (string-to-number (match-string 1))))))
-			   ;; Otherwise, fall-back to provided number.
-			   (car org-image-actual-width)))
-			 ((numberp org-image-actual-width)
-			  org-image-actual-width)))
-		       (old (get-char-property-and-overlay
-			     (org-element-property :begin link)
-			     'org-image-overlay)))
-		   (if (and (car-safe old) refresh)
-		       (image-refresh (overlay-get (cdr old) 'display))
-		     (let ((image
-			    (create-image (if (org-file-remote-p file)
-					      (let* ((tramp-tmpdir (concat
-								    (if (featurep 'xemacs)
-									(temp-directory)
-								      temporary-file-directory)
-								    "/tramp"
-								    (file-name-directory (expand-file-name file))))
-						     (newname (concat
-							       tramp-tmpdir
-							       (file-name-nondirectory (expand-file-name file)))))
-						(make-directory tramp-tmpdir t)
-						(if (file-newer-than-file-p file newname)
-						    (copy-file file newname t t))
-						newname)
-					    file)
-					  (and width 'imagemagick)
-					  nil
-					  :width width)))
-		       (when image
-			 (let* ((link
-				 ;; If inline image is the description
-				 ;; of another link, be sure to
-				 ;; consider the latter as the one to
-				 ;; apply the overlay on.
-				 (let ((parent
-					(org-element-property :parent link)))
-				   (if (eq (org-element-type parent) 'link)
-				       parent
-				     link)))
-				(ov (make-overlay
-				     (org-element-property :begin link)
-				     (progn
-				       (goto-char
-					(org-element-property :end link))
-				       (skip-chars-backward " \t")
-				       (point)))))
-			   (overlay-put ov 'display image)
-			   (overlay-put ov 'face 'default)
-			   (overlay-put ov 'org-image-overlay t)
-			   (overlay-put
-			    ov 'modification-hooks
-			    (list 'org-display-inline-remove-overlay))
-			   (push ov org-inline-image-overlays)))))))))))))))
