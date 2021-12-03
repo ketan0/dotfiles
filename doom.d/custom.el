c81d2e3dd4ddf84196f7b70d6ad6418dd9f4b0ba
Ketan Agrawal <agrawalk@stanford.edu>
2021-12-03 01:30:06 -0800

    new stuff

---
+++ doom.d/custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values '((ketan0/org-html-auto-export-mode . t))))
+(custom-theme-set-faces!
+ 'leuven
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-block :extend t :background "#EAEAFF" :foreground "#000088")
+ '(org-block-begin-line :extend t :background "#EAEAFF" :foreground "#008ED1")
+ '(org-block-end-line :extend t :background "#EAEAFF" :foreground "#008ED1"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
